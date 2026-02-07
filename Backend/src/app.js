0af04367c6eb83ee67bb6d952f2542c63f1f54ce
Abhay Mulani <2k22.aiml.2212656@gmail.com>
2026-02-07 20:28:14 +0530

    Refactor app.js to include CORS and JSON parsing

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,9 +1,18 @@
-const express =require('express');
-const aiRoutes= require('./routes/ai.routes')
-const app=express()
+const express = require('express');
+const aiRoutes = require('./routes/ai.routes')
+const cors = require('cors')
 
-app.get('/',(req,res)=>{
-    res.send('Hello World!')
+const app = express()
+
+app.use(cors())
+
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
 })
-app.use('/ai',aiRoutes)
-module.exports=app
+
+app.use('/ai', aiRoutes)
+
+module.exports = app
